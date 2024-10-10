4d7023f14fefa2fae4a7abdfaad3b1a003209917
TomerHFB <158162596+TomerHFB@users.noreply.github.com>
2024-10-10 17:11:47 +0300

    fixes

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,17 @@
 export class CallFromProxy {
-    vaultAccountId: string;
-    method: string;
-    realAddress: string;
-    proxyType?: string;
-    proxyCallParams?: any[]
+  vaultAccountId: string;
+  method: string;
+  realAddress: string;
+  proxyType?: string;
+  proxyCallParams?: any[];
+}
+
+export enum ProxyTypesEnum {
+  Any = "Any",
+  NonTransfer = "NonTransfer",
+  Governance = "Governance",
+  Staking = "Staking",
+  IdentityJudgement = "IdentityJudgement",
+  Auction = "Auction",
+  NominationPools = "NominationPools",
 }
